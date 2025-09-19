295c251b296a90e6408e98cd794784e59fc09ac9
Adi <afcadinho@gmail.com>
2025-09-19 21:32:06 +0200

    fix users column, pvp xperiece

--- discord-bot/db_backups/afc_db_backup_20250916151116.sql
+++ discord-bot/db_backups/afc_db_backup_20250916151116.sql
@@ -171,32 +171,39 @@ COPY public.loans (id, pokemon_id, user_id, borrowed_at, returned_at) FROM stdin
 --
 
 COPY public.pokemon (id, name, ability, nature, tier, discord_link, always_stored, loaned, in_storage, created_at) FROM stdin;
-1	Slowking-Galar	Regenerator	Sassy	ou	https://discord.com/channels/1302588750630621184/1399200070439800854	f	f	f	2025-08-04 18:39:13.524884+00
-3	Alomomola	Regenerator	Relaxed	ou	https://discord.com/channels/1302588750630621184/1399200854619193364	f	f	f	2025-08-04 18:42:51.434982+00
-5	Iron Valiant	Quark Drive	Naive	ou	https://discord.com/channels/1302588750630621184/1399202526070308976	f	f	f	2025-08-04 18:45:16.987116+00
-7	Ceruledge	Weak Armor	Adamant	ou	https://discord.com/channels/1302588750630621184/1399203649300987974	f	f	f	2025-08-04 18:46:34.149021+00
-10	Pelipper	Drizzle	Bold	ou	https://discord.com/channels/1302588750630621184/1399266879725637632	f	f	f	2025-08-04 18:50:12.785001+00
 11	Skeledirge	Unaware	Careful	uu	https://discord.com/channels/1302588750630621184/1399267801117491241	f	f	f	2025-08-04 18:50:48.870282+00
-12	Politoed	Drizzle	Bold	uu	https://discord.com/channels/1302588750630621184/1399420573490348215	f	f	f	2025-08-04 18:51:35.339213+00
-13	Gardevoir	Trace	Timid	uu	https://discord.com/channels/1302588750630621184/1399420705098956852	f	f	f	2025-08-04 18:56:26.687572+00
 14	Conkeldurr	Guts	Adamant	uu	https://discord.com/channels/1302588750630621184/1399460608897847537	f	f	f	2025-08-04 18:56:49.526052+00
-15	Gyarados	Moxie	Jolly	uu	https://discord.com/channels/1302588750630621184/1399460802121044173	f	f	f	2025-08-04 18:57:16.44296+00
 16	Darkrai	Bad Dreams	Timid	ou	https://discord.com/channels/1302588750630621184/1399596671025025097	f	f	f	2025-08-04 18:57:47.247322+00
-17	Barraskewda	Swift Swim	Adamant	ou	https://discord.com/channels/1302588750630621184/1399597038357844120	f	f	f	2025-08-04 18:58:42.386483+00
 18	Buzzwole	Beast Boost	Careful	uu	https://discord.com/channels/1302588750630621184/1399597312283508797	f	f	f	2025-08-04 18:59:57.186091+00
-19	Heatran	Flash Fire	Rash	ou	https://discord.com/channels/1302588750630621184/1399805873295196170	f	f	f	2025-08-04 19:00:22.53774+00
 21	Iron Bundle	Quark Drive	Timid	ou	https://discord.com/channels/1302588750630621184/1400078741245268089	f	f	f	2025-08-04 19:01:40.606322+00
 22	Dragapult	Infiltrator	Naive	ou	https://discord.com/channels/1302588750630621184/1400180293763076106	f	f	f	2025-08-04 19:02:10.427184+00
-24	Corviknight	Pressure	Impish	ou	https://discord.com/channels/1302588750630621184/1400181182410526841	f	f	f	2025-08-04 19:05:03.130401+00
-26	Great Tusk	Protosynthesis	Jolly	ou	https://discord.com/channels/1302588750630621184/1399199801291309136	f	f	f	2025-08-04 19:06:44.828108+00
-2	Bisharp	Defiant	Adamant	uu	https://discord.com/channels/1302588750630621184/1399201390617825280	f	f	f	2025-08-04 18:42:09.101679+00
-4	Kingdra	Swift Swim	Modest	uu	https://discord.com/channels/1302588750630621184/1399201738724212808	f	f	f	2025-08-04 18:43:55.832426+00
-8	Scizor	Technician	Brave	uu	https://discord.com/channels/1302588750630621184/1399229273235128453	f	f	f	2025-08-04 18:47:50.736275+00
-9	Amoongus	Regenerator	Bold	uu	https://discord.com/channels/1302588750630621184/1399245513198665870	f	f	f	2025-08-04 18:49:10.643092+00
+12	Politoed	Drizzle	Bold	uu	https://discord.com/channels/1302588750630621184/1399420573490348215	f	f	f	2025-08-04 18:51:35.339213+00
+13	Gardevoir	Trace	Timid	uu	https://discord.com/channels/1302588750630621184/1399420705098956852	f	f	f	2025-08-04 18:56:26.687572+00
+15	Gyarados	Moxie	Jolly	uu	https://discord.com/channels/1302588750630621184/1399460802121044173	f	f	f	2025-08-04 18:57:16.44296+00
 20	Annihilape	Defiant	Careful	uu	https://discord.com/channels/1302588750630621184/1400073663063265341	f	f	f	2025-08-04 19:00:58.425125+00
 23	Gengar	Cursed Body	Timid	uu	https://discord.com/channels/1302588750630621184/1400180887567597611	f	f	f	2025-08-04 19:04:21.011493+00
 25	Moltres-Galar	Berserk	Modest	uu	https://discord.com/channels/1302588750630621184/1400585815976968325	f	f	f	2025-08-04 19:05:57.377555+00
+31	Tyranitar	Sand Stream	Adamant	ou	https://discord.com/channels/1302588750630621184/1415391979402952795	f	f	f	2025-09-10 17:43:28.267987+00
+2	Bisharp	Defiant	Adamant	uu	https://discord.com/channels/1302588750630621184/1399201390617825280	f	f	f	2025-08-04 18:42:09.101679+00
+32	Archaludon	Stamina	Modest	ou	https://discord.com/channels/1302588750630621184/1415755252463829144	f	f	f	2025-09-11 17:47:26.953684+00
+8	Scizor	Technician	Brave	uu	https://discord.com/channels/1302588750630621184/1399229273235128453	f	f	f	2025-08-04 18:47:50.736275+00
+9	Amoongus	Regenerator	Bold	uu	https://discord.com/channels/1302588750630621184/1399245513198665870	f	f	f	2025-08-04 18:49:10.643092+00
+33	Glimmora	Toxic Debris	Timid	ou	https://discord.com/channels/1302588750630621184/1416150350049579088	f	f	f	2025-09-12 19:57:12.222561+00
 6	Garchomp	Rough Skin	Jolly	ou	https://discord.com/channels/1302588750630621184/1399203029282193579	t	f	t	2025-08-04 18:46:01.943531+00
+19	Heatran	Flash Fire	Rash	ou	https://discord.com/channels/1302588750630621184/1399805873295196170	t	f	t	2025-08-04 19:00:22.53774+00
+5	Iron Valiant	Quark Drive	Naive	ou	https://discord.com/channels/1302588750630621184/1399202526070308976	t	f	t	2025-08-04 18:45:16.987116+00
+4	Kingdra	Swift Swim	Modest	uu	https://discord.com/channels/1302588750630621184/1399201738724212808	f	f	f	2025-08-04 18:43:55.832426+00
+1	Slowking-Galar	Regenerator	Sassy	ou	https://discord.com/channels/1302588750630621184/1399200070439800854	f	f	f	2025-08-04 18:39:13.524884+00
+3	Alomomola	Regenerator	Relaxed	ou	https://discord.com/channels/1302588750630621184/1399200854619193364	f	f	f	2025-08-04 18:42:51.434982+00
+7	Ceruledge	Weak Armor	Adamant	ou	https://discord.com/channels/1302588750630621184/1399203649300987974	f	f	f	2025-08-04 18:46:34.149021+00
+10	Pelipper	Drizzle	Bold	ou	https://discord.com/channels/1302588750630621184/1399266879725637632	f	f	f	2025-08-04 18:50:12.785001+00
+17	Barraskewda	Swift Swim	Adamant	ou	https://discord.com/channels/1302588750630621184/1399597038357844120	f	f	f	2025-08-04 18:58:42.386483+00
+24	Corviknight	Pressure	Impish	ou	https://discord.com/channels/1302588750630621184/1400181182410526841	f	f	f	2025-08-04 19:05:03.130401+00
+26	Great Tusk	Protosynthesis	Jolly	ou	https://discord.com/channels/1302588750630621184/1399199801291309136	f	f	f	2025-08-04 19:06:44.828108+00
+27	Xurkitree	Beast Boost	Timid	uu	https://discord.com/channels/1302588750630621184/1405011627312545913	f	f	f	2025-08-13 02:16:01.49589+00
+28	Arctozolt	Slush Rush	Naive	ou	https://discord.com/channels/1302588750630621184/1405012301819412601	f	f	f	2025-08-13 02:18:15.767131+00
+29	Roaring Moon	Protosynthesis	Jolly	ou	https://discord.com/channels/1302588750630621184/1405261671374520472	f	f	f	2025-08-13 18:49:13.947565+00
+30	Zapdos-Galar	Defiant	Jolly	uu	https://discord.com/channels/1302588750630621184/1415383096806871100	f	f	f	2025-09-10 17:12:21.40187+00
 \.
 
 
@@ -206,6 +213,11 @@ COPY public.pokemon (id, name, ability, nature, tier, discord_link, always_store
 
 COPY public.users (id, discord_id, username, country_timezone, is_active, created_at) FROM stdin;
 1	195599180624822272	Adinho	Netherlands	t	2025-08-04 19:07:30.307698+00
+2	1407815102454960201	xiyou	China	t	2025-08-21 12:16:21.82665+00
+3	487615845951078414	iZandex	Spain	t	2025-08-27 18:30:07.65925+00
+4	205722519208984576	ImLeamox	Germany	t	2025-08-30 11:04:53.514964+00
+5	319455756699041792	Raehl	Brazil	t	2025-09-08 12:49:56.200869+00
+6	725375834202046496	Vysa	Germany	t	2025-09-08 14:49:08.445261+00
 \.
 
 
@@ -220,14 +232,14 @@ SELECT pg_catalog.setval('public.loans_id_seq', 1, false);
 -- Name: pokemon_id_seq; Type: SEQUENCE SET; Schema: public; Owner: afc_squad
 --
 
-SELECT pg_catalog.setval('public.pokemon_id_seq', 26, true);
+SELECT pg_catalog.setval('public.pokemon_id_seq', 33, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: afc_squad
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+SELECT pg_catalog.setval('public.users_id_seq', 6, true);
 
 
 --
